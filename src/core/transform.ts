c78b36f8d2006ce0fd5c0b98393c9bf7785b5f20
Moomyd <1345449751@qq.com>
2021-02-27 19:10:59 +0800

    feat(all the file): push all files
    ts implement axios

---
+++ src/core/transform.ts
@@ -0,0 +1,19 @@
+import { AxiosTransformer } from '../types'
+
+export default function transform(
+  data: any,
+  headers: any,
+  fns?: AxiosTransformer | AxiosTransformer[]
+): any {
+  if (!fns) {
+    return data
+  }
+  if (!Array.isArray(fns)) {
+    fns = [fns]
+  }
+  //链式转换函数调用
+  fns.forEach(fn => {
+    data = fn(data, headers)
+  })
+  return data
+}
